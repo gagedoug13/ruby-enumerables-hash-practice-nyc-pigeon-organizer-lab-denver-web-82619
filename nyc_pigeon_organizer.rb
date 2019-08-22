550c14a14f5056c44a99e9bb2fb17842a4530145
Gage Thornberry <gagedoug13@gmail.com>
2019-08-22 04:42:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
 def nyc_pigeon_organizer(data)
+  new_hash = {}
+  data.each do |colors_gender_lives, values|
+    values.each do |value, array|
+      array.each do |name|
+        if hash[name] == nil
+          hash[name] = {}
+          hash[name][colors_gender_lives] = []
+        else
+          hash[name][colors_gender_lives] = []
+        end
+      end
+    end
+  end
+  hash.each do |name, values|
+  values.each do |hashvalue, array|
+  data.each do |color_gender_lives, values|
+    values.each do |value, array|
 
+      array.each do |element|
+        if element == name && hashvalue == color_gender_lives
+          hash[name] [hashvalue] << value.to_s
+        end
+      end
+    end
+  end
+end
+end
+p new_hash    
 end
